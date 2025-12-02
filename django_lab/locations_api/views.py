552228d25ca3def636feb12e3c8e1813ecb797ed
Cody.McFarland <codymcfarland@protonmail.com>
2025-12-02 16:48:11 -0500

    save

--- django_lab/locations_api/views.py
+++ django_lab/locations_api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .serializers import locationSerializer
+from .models import location
+
+class LocationList(generics.ListCreateAPIView):
+    queryset = location.objects.all().order_by('id') # tell django how to retrieve all objects from the DB, order by id ascending
+    serializer_class = locationSerializer # tell django what serializer to use
+class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = location.objects.all().order_by('id')
+    serializer_class = locationSerializer
